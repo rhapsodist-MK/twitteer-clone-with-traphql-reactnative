d313c7ac5e21eb5fe12ccc1803c431f81a66bb6b
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-15 23:08:32 +0900

    setting mongodb

---
+++ server/config/db.js
@@ -0,0 +1,30 @@
+// const mongoose = require('mongoose')
+
+// // const constants = require('./constants')
+
+
+// mongoose.Promise = global.Promise
+
+// mongoose.set('debug', true)
+
+// try {
+//   mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+// } catch (err) {
+//   mongoose.createConnection(process.env.MONGODB_URI, { useMongoClient: true })
+// }
+
+// mongoose.connection
+//   .once('open', () => console.log('MongoDB Running'))
+//   .on('error', e => {
+//     throw e
+//   })
+
+
+const mongoose = require('mongoose')
+const connection = process.env.MONGODB_URI || 'mongodb://localhost:27017/tweet-development'
+const connectDb = () => {
+    return mongoose.connect(connection)
+}
+
+console.log('####################################')
+module.exports = connectDb
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+
+const connectDb = require('./config/db')
+require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.json())
 
-app.listen(port, err => console.log(`server is up in prot ${port}`))
+connectDb().then(async () => {
+  app.use(bodyParser.json())
+  
+
+
+  app.listen(port, err => console.log(`server is up in prot ${port}`))
+})
+.catch((ex) => {
+  console.err(ex.stack)
+  process.exit(1)
+})
